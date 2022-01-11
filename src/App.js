d15a1aba1319cb8a57abf4cb6533a13b817f50a4
Fluffy-Lion <leoncooper31@gmail.com>
2022-01-11 15:25:38 +0000

    example added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 import React from "react"
 
 class Clock extends React.Component {
+  state = {
+    date : new Date(),
+  }
+  seconds = 0
+  componentDidMount() {
+    // setInterval runs the code repeatedly
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    )
+    console.log("mounted")
+  }
 
+  componentWillUnmount() {
+    clearInterval(this.timerID)
+  }
+  componentDidUpdate() {
+    this.seconds += 1
+  }
 
-  // componentWillUnmount() {
-  //   clearInterval(this.timerID)
-  // }
-
-
-
-  
+  tick() {
+    this.setState({
+      date: new Date()
+    })
+  }
   render() {
     return (
         <div>
             <h1>hello world</h1>
-            <h2>it is </h2>
+            <h2>it is {this.state.date.toLocaleTimeString()}</h2>
             <h2>how many seconds have you been here for?: {this.seconds}</h2>
         </div>
     )
