a8a9ffeafe10d9997d0e6be6f4aa322cd986adf2
Fluffy-Lion <leoncooper31@gmail.com>
2022-01-11 15:21:24 +0000

    lifecycle activity

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Clock extends React.Component {
+
+
+  // componentWillUnmount() {
+  //   clearInterval(this.timerID)
+  // }
+
+
+
+  
+  render() {
+    return (
+        <div>
+            <h1>hello world</h1>
+            <h2>it is </h2>
+            <h2>how many seconds have you been here for?: {this.seconds}</h2>
+        </div>
+    )
+  }
 }
-
-export default App;
+export default Clock
